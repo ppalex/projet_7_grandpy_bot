ca32accc1ea0e5de174deec7c4eb93b934ed5ce8
ppalex <prietopantoja.alex@gmail.com>
2020-06-11 15:41:40 +0200

    add js file + bootstrap script

---
+++ flaskr/models.py
@@ -0,0 +1,12 @@
+class GoogleApi():
+    def __init__():
+        pass
+    
+class WikiApi():
+    def __init__():
+        pass
+    
+    
+class Parser():
+    def __init__():
+        pass
--- flaskr/run.py
+++ flaskr/run.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .views import view
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 app.register_blueprint(view)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
